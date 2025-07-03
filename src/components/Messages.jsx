3ac70b99e702c7f010f3f1be1c4bbef66669a10d
atharva <atharvasingh0072@gmail.com>
2025-07-03 22:37:57 +0530

    3 july 10:24 pm

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import Message from './Message'
+import React, { useEffect } from "react";
+import Message from "./Message";
+import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
+import { db } from "../firebase";
+
 
 const Messages = () => {
+  useEffect(() => {
+    const q = query(collection(db, "emails"), orderBy('createdAt', 'desc'));
+    const unsubscribe = onSnapshot(collection(db, "emails"), (snapshot) => {
+      const allEmails = snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      console.log(allEmails);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div>
       <Message />
     </div>
-  )
-}
+  );
+};
 
-export default Messages
+export default Messages;
